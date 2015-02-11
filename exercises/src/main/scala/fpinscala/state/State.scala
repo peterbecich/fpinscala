b38ddbc09b20a032a30c89504f84698a5eb039a5
Peter Becich <peterbecich@gmail.com>
2015-02-11 00:44:07 -0800

    state

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -20,6 +20,7 @@ object RNG {
   type Rand[+A] = RNG => (A, RNG)
 
   val int: Rand[Int] = _.nextInt
+  def randInt: Rand[Int] = (rng: RNG) => rng.nextInt
 
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
@@ -30,23 +31,127 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (nextInt, nextRNG) = rng.nextInt
+    (nextInt.abs, nextRNG)
+  }
+                                             //RNG.map(rng)(_.abs)
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (nextInt, nextRNG) = rng.nextInt
+    val nextDouble = nextInt.doubleValue/Int.MaxValue
+    (nextDouble, nextRNG)
+  }
+  def randDouble: Rand[Double] = (rng: RNG) => double(rng)
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    // val (nextInt, secondRNG) = rng.nextInt
+    // val (nextDouble, thirdRNG) = RNG.double(secondRNG)
+    // ((nextInt, nextDouble), thirdRNG)
+    val intDoubleRand: Rand[(Int, Double)] = 
+      RNG.map2(
+        (rngA: RNG) => rngA.nextInt, 
+        (rngB: RNG) => RNG.double(rngB)
+      )((int: Int, dbl: Double) => (int, dbl))
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+    intDoubleRand(rng)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val (nextDouble, secondRNG) = RNG.double(rng)
+    val (nextInt, thirdRNG) = secondRNG.nextInt
+    ((nextDouble, nextInt), thirdRNG)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    /*
+     It would be nice to figure out this solution with flatMap,
+     fold, etc.
+     */
+    // for(i<-1:3;
+    //   val (nextDouble, nextRNG){
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+    // }
+    val ll = List.fill(3)(randDouble)
+
+    ???
+    
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    val ll = List.fill(count)(randInt)
+
+    // ll.foldRight(z: B)(op: Function2[Function1[Int], B, B])
+
+    ll.foldRight((List[Int](), rng))(
+      (nextRand: Rand[Int], l2: (List[Int], RNG)) => {
+        val (nextInt, nextRNG) = nextRand(l2._2)
+        (nextInt :: l2._1, nextRNG)
+      }
+    )
+  }
+  def list[A](count: Int)(rng: RNG)(rand: Rand[A]): (List[A], RNG) = {
+    val ll = List.fill(count)(rand)
+    ll.foldRight((List[A](), rng))(
+
+// [error] Note: Tuples cannot be directly destructured in method or function parameters.
+// [error]       Either create a single parameter accepting the Tuple1,
+// [error]       or consider a pattern matching anonymous function: `{ case (param1, param1) => ... }
+// [error]       (nextRand: Rand[A], (prevAList, prevRNG): (List[A], RNG)) => {
+// [error]                                               ^
+
+
+//       (nextRand: Rand[A], (prevAList, prevRNG): (List[A], RNG)) => {
+//         val (nextA, nextRNG) = nextRand(prevRNG)
+//         (nextA :: prevAList, nextRNG)
+//       }
+
+      (nextRand: Rand[A], l2: (List[A], RNG)) => {
+        val (nextA, nextRNG) = nextRand(l2._2)
+        (nextA :: l2._1, nextRNG)
+      }
+
+    )
+  }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = 
+  {
+    // Return function RNG => (C, RNG)
+    (rng: RNG) => {
+      val (raValue, raRNG) = ra(rng)
+      val (rbValue, rbRNG) = rb(raRNG)
+      (f(raValue, rbValue), rbRNG)
+    }
+
+  }
+
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+
+    // difference between
+    // (g: A) => Rand[B]
+    // and
+    // g: A => Rand[B]
+
+    // Rand[A] = RNG => (A, RNG)
+    // Rand[B] = RNG => (B, RNG)
+    // g = A => (RNG => (A, RNG))
+
+    (rng: RNG) => {
+      val (valueA, rngA) = f(rng)
+      val randB = g(valueA)
+      randB(rngA)
+    }
+  }
+
+  def mapWithFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
